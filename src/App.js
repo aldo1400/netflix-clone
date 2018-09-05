980fd2c0a293305178651696774c0ca96f3fe2f3
Aldo Choque <achoque1400@gmail.com>
2018-09-05 13:05:21 -0500

    Swiper

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import List from './components/List'
+import Slide from './components/Slider'
 
 export default class App extends Component {
   render() {
     return (
       <View style={[{flex:1},styles.container]}>
+        <Slide/>
         <List />
       </View>
     );
